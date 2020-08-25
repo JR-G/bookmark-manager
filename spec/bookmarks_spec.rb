ce7c4ef9e033ed8b11b8a81ad9d3c5b24b750de8
JRG <47917431+JR-G@users.noreply.github.com>
2020-08-25 15:40:27 +0100

    Added a failing test for the create method

--- spec/bookmarks_spec.rb
+++ spec/bookmarks_spec.rb
@@ -1,19 +1,26 @@
 require 'bookmarks'
 
 describe Bookmarks do
-  before do
-    connection = PG.connect(dbname: 'bookmark_manager_test')
-
-    connection.exec("INSERT INTO bookmarks (url) VALUES ('www.google.com');")
-    connection.exec("INSERT INTO bookmarks (url) VALUES ('www.bbc.com');")
-    connection.exec("INSERT INTO bookmarks (url) VALUES ('www.twitter.com');")
-  end
 
   describe '.all' do
     it 'returns a list of bookmarks' do
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('www.google.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('www.bbc.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('www.twitter.com');")
+
       expect(described_class.all).to include 'www.google.com'
       expect(described_class.all).to include 'www.bbc.com'
       expect(described_class.all).to include 'www.twitter.com'
+    end
+  end
+
+  describe '.create' do
+    it 'creates a new bookmark' do
+      described_class.create(url: 'wwww.mytestsite.com')
+
+      expect(described_class.all).to include 'www.mytestsite.com'
     end
   end
 
