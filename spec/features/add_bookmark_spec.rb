4b0888e7b0de64d2a66beae2ca09c17c23fb8daf
JRG <47917431+JR-G@users.noreply.github.com>
2020-08-25 16:12:12 +0100

    Added a failing test for the title field and added a second sql query

--- spec/features/add_bookmark_spec.rb
+++ spec/features/add_bookmark_spec.rb
@@ -1,9 +1,10 @@
 feature 'Adding bookmarks' do
   scenario 'user can submit bookmarks' do
     visit '/bookmarks/new'
-    fill_in 'url', with: 'www.mytestsite.com'
+    fill_in :url, with: 'www.mytestsite.com'
+    fill_in :title, with: 'Test Site'
     click_button 'Submit'
 
-    expect(page).to have_content 'www.mytestsite.com'
+    expect(page).to have_link 'Test Site', href: 'www.mytestsite.com'
   end
 end
