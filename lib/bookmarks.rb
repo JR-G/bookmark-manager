4efb9154d12491777ec8457cbed6096371d26cdf
JRG <47917431+JR-G@users.noreply.github.com>
2020-08-26 17:45:29 +0100

    Passed the unit test to update bookmarks

--- lib/bookmarks.rb
+++ lib/bookmarks.rb
@@ -31,7 +31,8 @@ class Bookmarks
     end
 
     result = connection.exec(
-      "INSERT INTO bookmarks (title, url) VALUES('#{title}', '#{url}') RETURNING id, title, url;"
+      "INSERT INTO bookmarks (title, url) VALUES('#{title}', '#{url}') 
+        RETURNING id, title, url;"
     )
     Bookmarks.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
   end
@@ -42,6 +43,21 @@ class Bookmarks
     else
       connection = PG.connect(dbname: 'bookmark_manager')
     end
+
     connection.exec("DELETE FROM bookmarks WHERE id = #{id}")
+  end
+
+  def self.update(id:, url:, title:)
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+    else
+      connection = PG.connect(dbname: 'bookmark_manager')
+    end
+
+    result = connection.exec(
+      "UPDATE bookmarks SET url = '#{url}', title = '#{title}' WHERE id = '#{id}' 
+        RETURNING id, title, url;"
+    )
+    Bookmarks.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
   end
 end
